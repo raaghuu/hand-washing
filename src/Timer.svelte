0fb1877b9360ca5aec4e190f595240dcd50a5ebd
Raghav Sharma <sharma.raghav34@gmail.com>
2020-11-11 19:59:56 +0530

    here we go again... HTML committed

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -2,6 +2,22 @@
   import ProgressBar from './ProgressBar.svelte';
 </script>
 
-<p>Timer Component</p>
+<style>
+  h2 {
+    margin: 0;
+  }
+
+  .start {
+    background-color: rgb(154, 73, 73);
+    width: 100%;
+    margin: 10px 0;
+  }
+</style>
+
+<div bp="grid">
+  <h2 bp="offset-5@md 4@md 12@sm">Seconds Left:</h2>
+</div>
 
 <ProgressBar />
+
+<button class="start">Start</button>
