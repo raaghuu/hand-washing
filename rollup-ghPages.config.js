2ce9a94452caa974a5d05ac1a2b0c360e0ef7652
Raghav Sharma <sharma.raghav34@gmail.com>
2020-11-15 00:08:38 +0530

    bifurcated rollup cnfigs for simple build and build with publish to GH pages

--- rollup-ghPages.config.js
+++ rollup-ghPages.config.js
@@ -74,7 +74,11 @@ export default {
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
-    production && terser(),
+    production &&
+      terser() &&
+      ghPages.publish('public', (err) => {
+        console.log('published to GitHub', err);
+      }),
   ],
   watch: {
     clearScreen: false,
